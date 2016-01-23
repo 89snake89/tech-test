4e9e00ec09b3fe690915467980ae41f027261c48
Fabio <bottanfabio@hotmail.it>
2016-01-23 19:41:39 +0100

    Created Person module

---
+++ module/Person/config/module.config.php
@@ -0,0 +1,32 @@
+<?php
+return array(
+		'controllers' => array(
+				'invokables' => array(
+						'Person\Controller\Person' => 'Person\Controller\PersonController',
+				),
+		),
+		// The following section is new and should be added to your file
+		'router' => array(
+				'routes' => array(
+						'person' => array(
+								'type'    => 'segment',
+								'options' => array(
+										'route'    => '/person[/:action][/:id]',
+										'constraints' => array(
+												'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+												'id'     => '[0-9]+',
+										),
+										'defaults' => array(
+												'controller' => 'Person\Controller\Person',
+												'action'     => 'index',
+										),
+								),
+						),
+				),
+		),
+		'view_manager' => array(
+				'template_path_stack' => array(
+						'person' => __DIR__ . '/../view',
+				),
+		),
+);
---
+++ module/Person/src/Person/Controller/PersonController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Person\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class PersonController extends AbstractActionController{
+	
+	public function indexAction(){
+		
+	}
+	
+	public function addAction(){
+		
+	}
+	
+	public function editAction(){
+		
+	}
+	
+	public function deleteAction(){
+		
+	}
+}
